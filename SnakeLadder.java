eeafc3e0fc55e2add0624c0ba6cc24bc3737849a
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-08 23:51:54 +0530

    Snake&Ladder UC1

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.day4SnakeLadder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		
+		int playerPosition = 0;
+		System.out.println("Initial position of Player is : " + playerPosition);
+	}
+}
