9cf8f8e334866c5f438bcfdba9fe879d86ffc177
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-08 23:59:15 +0530

    SnakeLadde UC2

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,9 +2,18 @@ package com.day4SnakeLadder;
 
 public class SnakeLadder {
 
+	private static int RandomDieValue() {
+		int RandomValue = (int) Math.floor((Math.random()*6)+1);
+		return RandomValue;
+	}
+	
 	public static void main(String[] args) {
 		
 		int playerPosition = 0;
+		int numberOnDie = 0;
 		System.out.println("Initial position of Player is : " + playerPosition);
+		numberOnDie = RandomDieValue();
+		System.out.println("Number on die is : " + numberOnDie);
+	
 	}
 }
